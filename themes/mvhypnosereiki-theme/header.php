e39067faa73c2016c840cd63f82140289481b50d
Julien Auger <julien.auger37@laposte.net>
2025-02-10 17:09:13 +0100

    fix(animation): removing reveal on scroll

--- themes/mvhypnosereiki-theme/header.php
+++ themes/mvhypnosereiki-theme/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
   <?php wp_head(); ?>
   <script defer async src='https://cdn.trustindex.io/loader.js?3a9d4524097391571536ba54c88'></script>
   <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
